b6c56f0145fcaba1bf1f45566d62b789b9fe9f9e
kasra torabi <Kasra.torabiii@gmail.com>
2025-02-08 15:38:26 +0330

    restarted

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginPrettier.configs.recommended, // Enable Prettier as an ESLint plugin
+  configPrettier, // Disable conflicting rules between ESLint and Prettier
+];
---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+
+export const metadata = {
+  title: "Home",
+  description: "Created by Kasra Torabi",
+  Image: "./favicon.ico",
+};
+
+
+const Home = () => {
+  return (
+    <div className="text-normal flex justify-center pt-10">
+      <h1>
+        HI !
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
